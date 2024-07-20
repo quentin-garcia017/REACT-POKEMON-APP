79d2fa8401f20f6fd04e5578764d06f8496eea29
quentin-garcia017 <quentin.garcia17@orange.fr>
2024-07-20 15:34:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import PokemonList from "./pages/pokemon-list";
+
+const App: FunctionComponent = () => {
+  return <PokemonList />;
+};
+
+export default App;
