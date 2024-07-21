4c3fe1ecaf72127fc66d2c0dbb8fb28890e6eaeb
quentin-garcia017 <quentin.garcia17@orange.fr>
2024-07-21 10:53:00 +0200

    feat: AJout route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React, { FunctionComponent } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonsDetail from "./pages/pokemon-detail";
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 const App: FunctionComponent = () => {
-  return <PokemonList />;
+  return (
+    <Router>
+      <div>
+        {/* La barrde de navigation comme à toutes les pages est ici */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+        {/* Le système de gestion des routes de notre application est ici */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/:id" component={PokemonsDetail} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
